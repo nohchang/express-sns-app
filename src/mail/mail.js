f714bccc3362f9c74051f78bd5f3111a655c1fcd
nohchang <nchq2123@naver.com>
2023-08-14 18:24:08 +0900

    express-passport-app 이어서

---
+++ src/mail/mail.js
@@ -0,0 +1,56 @@
+const mailer = require('nodemailer');
+const goodbye = require('./goodbye_template');
+const welcome = require('./welcome_template');
+
+const getEmailData = (to, name, template) => {
+  let data = null;
+
+  switch (template) {
+    case "welcome":
+      data = {
+        // from: '보내는 사람 이름 <userId@gmail.com>',
+        to,
+        subject: `Welcome ${name}`,
+        html: welcome()
+      }
+      break;
+
+    case "goodbye":
+      data = {
+        // from: '보내는 사람 이름 <userId@gmail.com>',
+        to,
+        subject: `Goodbye ${name}`,
+        html: goodbye()
+      }
+      break;
+  
+    default:
+      data;
+  }
+
+  return data;
+}
+
+const sendMail = (to, name, type) => {
+  const transporter = mailer.createTransport({
+    service: 'Gmail',
+    auth: {
+      user: 'limlamz2123@gmail.com',
+      pass: process.env.EMAIL_PASSWORDD
+    }
+  })
+  
+  const mail = getEmailData(to, name, type);
+  
+  transporter.sendMail(mail, (error, response) => {
+    if (error) {
+      console.log(error);
+    } else {
+      console.log('email sent successfully');
+    }
+  
+    transporter.close();
+  })
+}
+
+module.exports = sendMail;
